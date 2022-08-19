9090babe9c3a1ed3a260fb097bbe3bf4fc86018d
Emer John Niones <nionesemerjohn@gmail.com>
2022-08-19 16:27:14 +0800

    done added error handling on request

--- app/api/rawg/request.rb
+++ app/api/rawg/request.rb
@@ -10,6 +10,9 @@ module Rawg
                 url: "#{BASE_URL}#{endpoint}?key=#{TOKEN}",
                 headers: {'Content-Type' => 'application/json'}
             )
+            { code: result.code, status: 'Success', data: JSON.parse(result.body) }
+        rescue RestClient::ExceptionWithResponse => error
+            { code: error.http_code, status: error.message, data: Errors.map(error.http_code) }
         end
     end
 end
