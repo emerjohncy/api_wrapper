76f07a6b66b2e5686fac9a19377e624b5216d2f2
Emer John Niones <nionesemerjohn@gmail.com>
2022-08-19 17:11:06 +0800

    add creators which get list of game creators

--- app/api/rawg/client.rb
+++ app/api/rawg/client.rb
@@ -3,5 +3,9 @@ module Rawg
         def self.creator_roles
             response = Request.call(http_method: 'get', endpoint: "/creator-roles")
         end
+        
+        def self.creators
+            response = Request.call(http_method: 'get', endpoint: "/creators")
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   namespace :api do
     get '/creator_roles' => 'creator_roles#index'
+    get '/creators' => 'creators#index'
   end
 end
